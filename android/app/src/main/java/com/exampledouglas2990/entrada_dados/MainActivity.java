d190734ae63e14288db6eb854d445707e8a533d1
Douglas <douglas2990@hotmail.com>
2020-11-05 13:54:22 -0300

    Initial commit

---
+++ android/app/src/main/java/com/exampledouglas2990/entrada_dados/MainActivity.java
@@ -0,0 +1,6 @@
+package com.exampledouglas2990.entrada_dados;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:entrada_dados/CampoTexto.dart';
+import 'package:flutter/material.dart';
+//9 87455776
+import 'EntradaCheckBox.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+       // home: CampoTexto(),
+        home: EntradaCheckBox(),
+
+      )
+  );
+}
